4212a0d1c73a063dd15991b3bcd3490d981a3066
Fatih <fatihcihan666@gmail.com>
2024-01-13 21:58:12 +0300

    improved design, imported BookList css file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './BookList.css';
 import Book from './Book';
 
 class BookList extends React.Component {
@@ -8,25 +9,22 @@ class BookList extends React.Component {
             return <Book book={book}
                 key={i} />
         });
-        return <div>{bookList}</div>
-
+        return (
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">Portfolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+                        {bookList}
+                    </div>
+                </div>
+            </section>
+        )
     }
 }
 
 export default BookList;
 
 
-// <!-- Portfolio Grid-->
-<section class="page-section bg-light" id="portfolio">
-    <div class="container">
-        <div class="text-center">
-            <h2 class="section-heading text-uppercase">Portfolio</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-sm-6">
-                
-            </div>
-        </div>
-    </div>
-</section>
