83f0f4e0ca5a154329169347a9f00607e9170f47
Fatih <fatihcihan666@gmail.com>
2024-01-14 00:26:47 +0300

    removed state (because information comes from BookContext), cleaned props in BookList component

--- src/components/App.js
+++ src/components/App.js
@@ -3,70 +3,8 @@ import BookList from './BookList'
 
 class App extends React.Component {
 
-    state = {
-        books: [
-            {
-                title: "Cesur Yeni Dünya",
-                author: "Aldous Huxley",
-                pageNum: 312,
-                imageURL: "https://image01.idefix.com/resize/800/0/product/209086/cesur-yeni-dunya-6436a59ba56df.jpg",
-                topic: "Gerek konuyu ele alış biçimi gerek hayal dünyasının zenginliği ve kara mizah anlayışı sayesinde tüm dünyayı büyülemeyi başaran usta yazar Huxley’nin eleştirel bir bakış açısıyla karamsar bir dünya projeksiyonu sunduğu romanındaki tespitleri, günümüz dünyasında başucu rehberi özelliği taşıyor."
-            },
-            {
-                title: "Fahrenheit 451",
-                author: "Ray Bradbury",
-                pageNum: 226,
-                imageURL: "https://image01.idefix.com/resize/800/0/product/367120/fahrenheit-451-64395084b2e53.jpg",
-                topic: "Distopik eserindeki kurgusunu kitap okumanın yasaklandığı, insanların hayatı sorgulamayı bıraktığı ve sadece tüketim için yaşadığı bir düzen üzerine kuruyor."
-            },
-            {
-                title: "Kırmızı Pazartesi",
-                author: "Gabriel Garcia Marquez",
-                pageNum: 296,
-                imageURL: "https://i.idefix.com/cache/600x600-0/originals/0000000064101-1.jpg",
-                topic: "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor."
-            },
-            {
-                title: "Şeker Portakalı",
-                author: "Jose Muro de Vasconselos",
-                pageNum: 200,
-                imageURL: "https://i.idefix.com/cache/600x600-0/originals/0000000064031-1.jpg",
-                topic: "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor."
-            },
-            {
-                title: "En Uzun Yüzyıl",
-                author: "İlber Ortaylı",
-                pageNum: 296,
-                imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001744876001-1.jpg",
-                topic: "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor."
-            },
-            {
-                title: "Dracula",
-                author: "Bram Stoker",
-                pageNum: 200,
-                imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001828853001-1.jpg",
-                topic: "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor."
-            },
-            {
-                title: "Karamazov Kardeşler",
-                author: "Fyodor Mihayloviç Dostoyevski",
-                pageNum: 200,
-                imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001803800001-1.jpg",
-                topic: "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor."
-            },
-            {
-                title: "Sultanın Korsanları",
-                author: "Emrah Safa Gürkan",
-                pageNum: 296,
-                imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001780787001-1.jpg",
-                topic: "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor."
-            }
-
-        ]
-    }
-
     render() {
-        return <div><BookList books={this.state.books} /></div>
+        return <div><BookList /></div>
     }
 }
 
