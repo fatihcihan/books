0076add30b97ee3ce62970a5bfcf4cdcd78e9afa
Fatih <fatihcihan666@gmail.com>
2024-01-13 21:42:10 +0300

    The design required for the component has been created

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -14,3 +14,19 @@ class BookList extends React.Component {
 }
 
 export default BookList;
+
+
+// <!-- Portfolio Grid-->
+<section class="page-section bg-light" id="portfolio">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Portfolio</h2>
+            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                
+            </div>
+        </div>
+    </div>
+</section>
